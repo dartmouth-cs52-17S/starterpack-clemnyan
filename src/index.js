3ea467161956ea5fa07c731c59a50cb188e3b122
clemnyan <clemnyan@gmail.com>
2017-04-14 03:26:33 -0400

    new changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const $ = require('jquery');
+// change require to es6 import style
 
-$('#main').html('Here we go!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = setInterval(function(){ myTimer() }, 1000);
+let b=0;
+
+function myTimer() {
+  var x = document.body;
+  b++;
+  document.getElementById("main").innerHTML=("You've been on this page for "+b+" seconds.. This will be a simple counter");
+}
